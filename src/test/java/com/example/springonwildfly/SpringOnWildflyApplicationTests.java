bff5c8aaec25fe030e6be883024b1a62f10c2cd5
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2023-06-04 16:43:44 +0400

    Initial commit

---
+++ src/test/java/com/example/springonwildfly/SpringOnWildflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springonwildfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnWildflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
